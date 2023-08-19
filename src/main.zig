26ef8f0786df0847d81212f44649a6902d7c924d
Nathan Michaels <nathan@nmichaels.org>
2023-08-20 00:13:15 +0200

    Use better name for add.

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add_i32(a: i32, b: i32) i32 {
+export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
 test "basic add functionality" {
-    try testing.expect(add_i32(3, 7) == 10);
+    try testing.expect(add(3, 7) == 10);
 }
